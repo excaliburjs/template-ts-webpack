058612bf08686fbeeb3726a3c6b4f49ff278e039
Kamran Ayub <kamran.ayub@gmail.com>
2017-01-01 17:24:52 -0600

    Change fontsize

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import * as ex from 'excalibur';
 
 var game = new ex.Engine({ width: 500, height: 500 });
-var hello = new ex.Label('Hello World', 100, 100, 'Arial 20px');
+var hello = new ex.Label('Hello World', 100, 100, 'Arial');
 hello.color = ex.Color.Red;
+hello.fontSize = 20;
 
 game.add(hello);
 game.start();
