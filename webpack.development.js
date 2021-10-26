b19a46de0b2f30120fd95c0aee28221546d2b3fe
Kamran Ayub <kamran.ayub@gmail.com>
2021-10-26 08:56:33 -0500

    use static.directory

--- webpack.development.js
+++ webpack.development.js
@@ -5,6 +5,8 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist",
+    static: {
+      directory: "./dist",
+    },
   },
 });
